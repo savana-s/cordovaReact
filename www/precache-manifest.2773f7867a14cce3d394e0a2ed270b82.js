185c6cc0e2bc289bf689a9dfd3f2644d5d5263a7
savana-s <savanasathre@gmail.com>
2020-04-21 23:10:50 -0700

    cordovaReactStart
    Started cordova app with react

---
+++ www/precache-manifest.2773f7867a14cce3d394e0a2ed270b82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5865edff0bb1f572a24eab82ec97dfa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "417576d3ab86a8859bb9",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "972312ed369891d1f9b4",
+    "url": "./static/js/2.638f2ae3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.638f2ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417576d3ab86a8859bb9",
+    "url": "./static/js/main.f30f4559.chunk.js"
+  },
+  {
+    "revision": "5cd8cbddf36fef5f3770",
+    "url": "./static/js/runtime-main.996e16a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
